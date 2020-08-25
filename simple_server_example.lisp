3ea3d8722679e2a37af70b11479e4dac7e24f307
Sourav Datta <sdatta@planview.com>
2020-08-25 16:29:43 +0530

    Fix a tiny style issue

--- simple_server_example.lisp
+++ simple_server_example.lisp
@@ -1,8 +1,4 @@
 (ql:quickload :cl-insta)
 
-
-;; (setf cl-insta:*cl-insta-root* "/Users/sourav/quicklisp/local-projects/cl-insta")
-
-(setf cl-insta:*cl-insta-root* "C:/Users/soura/quicklisp/local-projects/cl-insta")
-
+(setf cl-insta:*cl-insta-root* "path-to-cl-insta")
 (cl-insta:start-server :port 3000)
