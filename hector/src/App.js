091f8bd6cfc2c880b5fc5caa817816703634054c
Sourav Datta <sdatta@planview.com>
2020-11-13 12:55:46 +0530

    Initial commit for hector-ui - new UI for CL

---
+++ hector/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
